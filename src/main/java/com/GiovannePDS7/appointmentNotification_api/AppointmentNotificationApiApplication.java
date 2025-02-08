3ef331400192c2923f57b33b1dc9d18b21078007
GiovannePDS7 <giovanne3282@gmail.com>
2025-02-08 19:41:57 -0300

    intial commit

---
+++ src/main/java/com/GiovannePDS7/appointmentNotification_api/AppointmentNotificationApiApplication.java
@@ -0,0 +1,13 @@
+package com.GiovannePDS7.appointmentNotification_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentNotificationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentNotificationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GiovannePDS7/appointmentNotification_api/AppointmentNotificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GiovannePDS7.appointmentNotification_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentNotificationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
