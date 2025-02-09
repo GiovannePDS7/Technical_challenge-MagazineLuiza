b7c01262e3c3f929b6db1abea2780f89eefd8c7c
GiovannePDS7 <giovanne3282@gmail.com>
2025-02-09 19:31:21 -0300

    add: cancellation methods

--- src/main/java/com/GiovannePDS7/appointmentNotification_api/Business/Mappers/IAgendamentoMapper.java
+++ src/main/java/com/GiovannePDS7/appointmentNotification_api/Business/Mappers/IAgendamentoMapper.java
@@ -4,12 +4,19 @@ import com.GiovannePDS7.appointmentNotification_api.Controller.dtos.In.Agendamen
 import com.GiovannePDS7.appointmentNotification_api.Controller.dtos.Out.AgendamentoRecordOut;
 import com.GiovannePDS7.appointmentNotification_api.Infrastructure.Entities.Agendamento;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 @Mapper(componentModel = SPRING)
 public interface IAgendamentoMapper {
-    Agendamento paraEntitty(AgendamentoRecordIn repository);
+
+    Agendamento paraEntity(AgendamentoRecordIn agendamento);
 
     AgendamentoRecordOut paraOut(Agendamento agendamento);
+
+    @Mapping(target = "dataHoraModificacao", expression = "java(LocalDateTime.now())")
+    @Mapping(target = "statusNotificacao", expression = "java(StatusNotificacaoEnum.CANCELADO)")
+    Agendamento paraEntityCancelamento(Agendamento agendamento);
 }
